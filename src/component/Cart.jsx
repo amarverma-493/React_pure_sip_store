472242256bcfc050bc690765ee945b09dd994dc3
amarverma-493 <amarverma493@gmail.com>
2025-09-02 17:43:39 +0530

    Initial commit

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState, useEffect } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState(() => {
+  const [cart, setCart] = useState(() => { 
     // Load cart from localStorage if available
     const storedCart = localStorage.getItem("cart");
     return storedCart ? JSON.parse(storedCart) : [];
